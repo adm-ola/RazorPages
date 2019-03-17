a2b66c4b8b1f671465879bc39c2a932ee226e3dd
olaogun4 <ola.ogun4@gmail.com>
2019-03-17 02:02:10 +0100

    Updated the seed data information

--- RazorPagesMovie/RazorPagesMovie/Models/SeedData.cs
+++ RazorPagesMovie/RazorPagesMovie/Models/SeedData.cs
@@ -39,8 +39,8 @@ namespace RazorPagesMovie.Models
 
                     new Movie
                     {
-                        Title = "Ghostbusters 2",
-                        ReleaseDate = DateTime.Parse("1986-2-23"),
+                        Title = "The Good, The Bad and the Ugly",
+                        ReleaseDate = DateTime.Parse("1975-2-23"),
                         Genre = "Comedy",
                         Price = 9.99M
                     },
