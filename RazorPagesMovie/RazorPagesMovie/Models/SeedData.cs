3abe8e108b2ba93964cd1e643c7f1323ac68b127
olaogun4 <ola.ogun4@gmail.com>
2019-03-17 03:22:46 +0100

    CheckIn Test again

--- RazorPagesMovie/RazorPagesMovie/Models/SeedData.cs
+++ RazorPagesMovie/RazorPagesMovie/Models/SeedData.cs
@@ -39,7 +39,7 @@ namespace RazorPagesMovie.Models
 
                     new Movie
                     {
-                        Title = "Ghostbusters 2",
+                        Title = "Good Bad Ugly",
                         ReleaseDate = DateTime.Parse("1986-2-23"),
                         Genre = "Comedy",
                         Price = 9.99M
