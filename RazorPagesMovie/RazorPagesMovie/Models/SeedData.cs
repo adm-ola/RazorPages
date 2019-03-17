b91774132b81e289c924b792192883997b6d7dbb
olaogun4 <ola.ogun4@gmail.com>
2019-03-17 03:50:35 +0100

    Lone Ranger Again

--- RazorPagesMovie/RazorPagesMovie/Models/SeedData.cs
+++ RazorPagesMovie/RazorPagesMovie/Models/SeedData.cs
@@ -31,7 +31,7 @@ namespace RazorPagesMovie.Models
 
                     new Movie
                     {
-                        Title = "The Lone Ranger",
+                        Title = "The Lone Ranger Again",
                         ReleaseDate = DateTime.Parse("1984-3-13"),
                         Genre = "Comedy",
                         Price = 8.99M
